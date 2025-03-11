1caaa840100e3d19fd6cb4bb8d448af1e3ade1a2
jspowley <jpowley@ualberta.ca>
2025-03-10 20:42:09 -0600

    working with a docker compose workflow

--- init/init.R
+++ init/init.R
@@ -9,3 +9,5 @@
 # docker network create selenium-net
 # docker run -d --name selenium --network selenium-net -p 4444:4444  -p 5900:5900  --shm-size=2g  selenium/standalone-chrome-debug:3.141.59-20210607
 # docker run -d --name novnc_with_net --network selenium-net -p 6900:6900  novnc
+
+# Working containers are selenium and novcn_with_net
