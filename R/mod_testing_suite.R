aa0ec18dfd22d2f2a91048647cb52d4b09381ed6
jspowley <jpowley@ualberta.ca>
2025-03-26 23:12:02 -0600

    added dynamic input system to testing suite

--- R/mod_testing_suite.R
+++ R/mod_testing_suite.R
@@ -11,9 +11,17 @@ mod_testing_suite_ui <- function(id) {
   
   ns <- NS(id)
   
-    bslib::nav_panel(
-      title = "Selector Flow Control"
+  bslib::nav_panel(
+    
+    title = "Testing and Export",
+    
+    bslib::layout_sidebar(
+      sidebar = bslib::sidebar(
+        width = 600, 
+        uiOutput(ns("selector_cards"))
+      )
     )
+  )
     
 }
     
@@ -23,9 +31,51 @@ mod_testing_suite_ui <- function(id) {
 mod_testing_suite_server <- function(id, r){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
+      
+      # Initiating dynamic input lists
+      input_array <- reactiveValues()
+    
       shiny::observeEvent(r$selector_list, {
-        saveRDS(r$selector_list, "temp.rds")
+        
+        selectors <- list()
+        
+        for(i in 1:length(r$selector_list)){
+          # Allows dynamic naming for lapply
+          n <- names(r$selector_list)[i]
+          selectors[i] <- list(r$selector_list[i])
+          names(selectors)[i] <- n
+        }
+        
+        saveRDS(selectors, "temp.rds")
+        print(str(selectors))
+        
+        output$selector_cards <- renderUI({
+          lapply(selectors, function(s) { #lapply handles the UI context better, based on a few stack overflow threads. Not my typical workflow but manages niche cases like this.
+            n <- names(s)[1]
+            print(n)
+            bslib::card(
+              bslib::card_header(paste0("Name: ",n)),
+              shiny::actionButton(ns(paste0("scrape_",n)), "Scrape")
+            )
+          })
+        })
       })
+      
+      # reference to EIAtools and the accompanying stack overflow post for flexible inputs
+      observe({ 
+        lapply(names(r$selector_list), function(n) {
+          id <- paste0("scrape_", n)
+          observeEvent(input[[id]], {
+            input_array$scrape <- list(id, input[[id]])
+          })
+        })
+      })
+      
+      observeEvent(input_array$scrape, {
+        print(str(input_array$scrape))
+      })
+      
+      #facet_select <- sapply(facets, function(f_name){input[[paste0("f_",f_name)]]}, simplify = FALSE)
   })
 }
     
