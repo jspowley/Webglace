e01cae51662a79ba288ddf1693f411dfd2cf4188
jspowley <jpowley@ualberta.ca>
2025-04-05 14:33:49 -0600

    rolling back export

--- R/data.R
+++ R/data.R
@@ -4,24 +4,21 @@
 #'
 #' @format An R6 class
 #' @source Website
-#' @export
 "post"
 
 #' post_title
 #'
-#' Selects a posts title
+#' Selects a post
 #'
 #' @format An R6 class
 #' @source Website
-#' @export
 "post_title"
 
 #' post_time
 #'
-#' Selects a posts posting date
+#' Selects a post
 #'
 #' @format An R6 class
 #' @source Website
-#' @export
 "post_time"
 
